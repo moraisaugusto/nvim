c6f78d3b27dcf63bfc64c61fc23f73853b55f121
Augusto Morais <augustofmorais@gmail.com>
2022-10-13 23:37:56 +0200

    chore: init repo with lua config

---
+++ user/plugins.lua
@@ -0,0 +1,29 @@
+local fn = vim.fn
+
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+---@diagnostic disable-next-line: missing-parameter
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path}
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+
+-- Install plugins here
+return require('packer').startup(function(use)
+  -- Plugin Manager
+  use 'wbthomason/packer.nvim'
+
+  -- LSP
+  use "neovim/nvim-lspconfig"
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require('packer').sync()
+  end
+end) 
