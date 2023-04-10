3e7bbcba3ad29aadd0533c3bfb54343f04dd8a58
Augusto Morais <augustofmorais@gmail.com>
2023-04-10 18:07:48 +0200

    chore: enable telescope binding

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -68,7 +68,12 @@ keymap("n", "<m-Right>", ":vertical resize +2<CR>", opts)
 -- NvimTree
 keymap("n", "<leader>k", ":NvimTreeToggle<cr>", opts)
 
+
 -- Telescope / Fuzzy
+--
+keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+keymap("n", "<leader>t", "<cmd>Telescope live_grep<cr>", opts)
+
 vim.api.nvim_set_keymap(
   "n",
   "<tab>",
