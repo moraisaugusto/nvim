f8e15e5ffdac8b498add6ad5f69db5bb547b3176
Augusto Morais <augustofmorais@gmail.com>
2023-04-11 22:15:46 +0200

    chore: add pylint as diagnostic

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -13,8 +13,8 @@ null_ls.setup {
   sources = {
     formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
     formatting.black.with { extra_args = { "--fast" } },
-    -- formatting.yapf,
     formatting.stylua,
     diagnostics.flake8,
+    diagnostics.pylint,
   },
 }
