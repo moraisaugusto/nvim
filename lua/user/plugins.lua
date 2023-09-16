fb4e78b0b8f4d6196c2a024f2cf300f9435a7527
Augusto Morais <augusto@morais.io>
2023-09-16 23:37:27 +0200

    chore: add chatgpt (tobeconfig); update nvim_tree config

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -108,6 +108,9 @@ return require('packer').startup(function(use)
   -- Lualine
   use "nvim-lualine/lualine.nvim"
 
+  -- ChatGPT
+  use "jackMort/ChatGPT.nvim"
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
